3b6d3722478816f21e112a728ac631d0e978829d
shahzad67432 <shaa1891640@gmail.com>
2024-02-16 20:51:12 +0500

    backend is almost working good

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { authMiddleware } = require('../middlewares/AuthMiddleware');
 const {User, balance} = require('../db/data')
 const accountRouter = express.Router()
+const  mongoose = require('mongoose')
 
 
 accountRouter.get('/userBalance', authMiddleware, async (req, res)=>{
@@ -16,32 +17,32 @@ accountRouter.post('/transfer', async (req, res)=>{
 
     session.startTransaction()
     const ammount = req.body.ammount
-    const receiver = req.body.receiver
-    const userId = await balance.findOne({userId: req.userId}).session(session)
-    if(ammount > userId.balance){
-        console.error("insufficient balance")
-    }
-    const checkReiceiver = await accountRouter.findOne({
-        userId: receiver
-    }).session(session)
-    if(!checkReiceiver){
-        res.json(
-            {
-                msg:"user not found"
-            }
-        )
-    }
-
-    const senderPaymentUpdated = await Account.UpdateOne({
+    const to = req.body.to
+    const account = await balance.findOne({userId: req.userId}).session(session)
+    // if(ammount > balance.balance){
+    //     console.error("insufficient balance")
+    // }
+    // const checkReiceiver = await balance.findOne({
+    //     account: to
+    // }).session(session)
+    // if(!checkReiceiver){
+    //     res.json(
+    //         {
+    //             msg:"user not found"
+    //         }
+    //     )
+    // }
+    const senderPaymentUpdated = await balance.updateOne({
         userId: req.userId
     },
     {
         $inc:{
             balance: -ammount
-        }
+        },
+        msg:console.log(`${req.userId}`)
     }).session(session)
-    const receiverPaymentUpdated = await Account.UpdateOne({
-        userId: receiver
+    const receiverPaymentUpdated = await balance.updateOne({
+        userId: to
     },{
         $inc:{
             balance: +ammount
