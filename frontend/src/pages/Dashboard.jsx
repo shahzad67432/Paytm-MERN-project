3cbd631f7ab18d5f0f2d7a78f050cd159d75bbf2
shahzad67432 <shaa1891640@gmail.com>
2024-02-19 17:02:21 +0500

    new pages added

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,21 @@
+import { Appbar } from "../components/Appbar";
+import { Balance } from "../components/Balance";
+import { Users } from "../components/Users";
+
 export function Dashboard() {
     return ( 
         <>
-            Dashboard
+            <div>
+                <Appbar />
+            </div>
+            <div className="m-5">
+                <div className="pt-6">
+                <Balance value={10000}/>
+                </div>
+                <div className="pt-8">
+                <Users/>
+                </div>
+            </div>
         </>
     );
 }
--- frontend/src/pages/SendMoney.jsx
+++ frontend/src/pages/SendMoney.jsx
@@ -1,7 +1,7 @@
 export function SendMoney() {
-    return (  
-        <>
-            money
-        </>
-    );
+  return (
+    <>
+      <SendMoney />
+    </>
+  );
 }
